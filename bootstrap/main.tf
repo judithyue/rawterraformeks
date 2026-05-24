e4e144697ce470bf7d932a7ae9c624e3128a7ef1
judith <denfish@gmail.com>
2026-05-24 18:22:20 +0800

    fix checkov 02

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -54,7 +54,6 @@ resource "aws_s3_bucket_public_access_block" "remote_state_block" {
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  # checkov:skip=CKV_AWS_119:Testing environment - AWS managed key is fine for locking
   name         = var.lock_table_name
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
@@ -62,6 +61,11 @@ resource "aws_dynamodb_table" "terraform_locks" {
   attribute {
     name = "LockID"
     type = "S"
+  }
+
+  # This native block fixes CKV_AWS_119 without needing a skip comment
+  server_side_encryption {
+    enabled = true
   }
 
   tags = merge(var.common_tags, {
