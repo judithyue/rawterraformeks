034cd9416d2324401002259d2a2dcfbcda43b8eb
judith <denfish@gmail.com>
2026-05-28 16:43:20 +0800

    fix bootstrap/main.tf for terrform fmt

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "remote_state" {
-  bucket       = var.backend_bucket
+  bucket        = var.backend_bucket
   force_destroy = true
 
   tags = merge(var.common_tags, {
